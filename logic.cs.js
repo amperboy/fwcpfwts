eb64402ac447d0a9d6fd3b81705dc2919b106e86
Marco <mgi@macbook.fritz.box>
2014-12-06 11:02:01 +0100

    Kampfergebnis bei Lebenssicht

--- logic.cs.js
+++ logic.cs.js
@@ -9,6 +9,76 @@ FWCPFWTS.prototype.freewar = function() {
 	
 FWCPFWTS.prototype.toolset = function() {}
 
+///////////////////////////////////////////////////
+
+function Toolset() {}
+Toolset.prototype.NIEDERLAGE = -1;
+Toolset.prototype.SIEG = 1;
+
+Toolset.prototype.styleResult = function(result, calcMsg) {
+	var msg = null;
+	var color = null;
+	
+	if(result == this.SIEG) {
+		msg = "SIEG";
+		color = "09E309";
+	}
+	else {
+		msg = "NIEDERLAGE";
+		color = "FC0000";
+	}
+
+	return {
+		"msg": calcMsg
+		, "result": result
+		, "resultObj": " <span style=\"color: #"+color+"\" title=\""+calcMsg+"\">"+msg+"</span>"
+	}
+}
+
+Toolset.prototype.fight = function(angreifer,verteidiger,resitenz) {
+	if ( angreifer == null || verteidiger == null ) {
+		return 0;
+	}
+	
+	var a_H = angreifer.health.current;
+	var a_A = 0; 		
+	var a_D = 0; 
+		
+	if (resitenz == 0) {
+		a_A += angreifer.offence.player;
+		a_D += angreifer.defence.player;
+	}
+		
+	a_A += angreifer.offence.weapon;
+	a_D += angreifer.defence.weapon;
+		
+	var v_H = verteidiger.health.current;
+	var v_A = verteidiger.offence.player + verteidiger.offence.weapon;
+	var v_D = verteidiger.defence.player + verteidiger.defence.weapon;
+
+		// Kampf berechnen
+		var nFv = (v_A-a_D)>=1 ? (v_A-a_D) : 1;
+		var Fv = a_H / (nFv);
+
+		var nFa = (a_A-v_D)>=1 ? (a_A-v_D) : 1;
+		var Fa = v_H / (nFa);
+				
+		var calsMsg = Fa+":"+Fv;		
+		
+		
+
+		if(Fa<=Fv) {
+			if(resitenz==2 && Fa>=1) {
+				// Superresistente NPCs heilen sich nach jedem Schlag vollstÃ¤ndig
+				return this.styleResult(this.NIEDERLAGE,calsMsg);
+			}
+			return this.styleResult(this.SIEG,calsMsg);	
+		}
+		else if(Fa>Fv){
+			return this.styleResult(this.NIEDERLAGE,calsMsg);	
+		}
+
+}
 
 
 ///////////////////////////////////////////////////
@@ -57,6 +127,7 @@ FreewarParser.prototype.doStuff = function() {
 			this.item();
 			break;
 		case "/freewar/internal/main.php":
+		case "/freewar/internal/fight.php":
 			this.main();
 			break;
 	}
@@ -68,7 +139,15 @@ FreewarParser.prototype.item = function() {
 	var numberExpression = /([.0-9]+)/;
 	
 	
-	var userData = {};
+	var userData = {
+		"name": null
+		, "xp": null
+		, "money": null
+		, "intelligence": null
+		, "health": null
+		, "offence": null			
+		, "defence": null
+	};
 	
 	var healthP = $("#listrow_lifep"); //Lebenspunkte
 	if (healthP.text().indexOf("Lebenspunkte") < 0) {
@@ -86,6 +165,11 @@ FreewarParser.prototype.item = function() {
 			var titleData = title.split(" (Erfahrung: ");
 			var name = titleData[0];
 			var xp = titleData[1].substring(0,titleData[1].length-1);
+			
+			if(xp.indexOf(" von ") > -1) {
+				var parts = xp.split(" von ");
+				xp = parts[0]*1;
+			}
 			
 			userData.name = name;
 			userData.xp = xp;
@@ -189,6 +273,8 @@ FreewarParser.prototype.ability = function() {
 
 
 FreewarParser.prototype.main = function() {
+	var siteParser = this;
+
 	this.receiveFromBackground("userData",function(value) {
 		if(value != null) {				
 			var health = value.health.current;
@@ -197,6 +283,86 @@ FreewarParser.prototype.main = function() {
 			$("p.personlistcaption:first").each(function(i,obj) {
 				obj.innerHTML += " (L: "+health+", A: "+offence+", V: "+defence+")";
 			});
+			
+			$("p.listusersrow").each(function(i,playerP) {
+				var inTable = false;
+				
+				$(playerP).find("table:first tr td:last").each(function(k,playerCell) {
+					inTable = true;
+					siteParser.playerCheck(value, playerCell);
+				});
+				
+				if(inTable == false) {
+					siteParser.playerCheck(value, playerP);				
+				}
+			});
 		}
 	});
+}
+
+FreewarParser.prototype.playerCheck = function(userdata, playerCell) {
+	var valueCurOfExpression = /([.0-9]+)([ ]*\/[ ]*)([.0-9]+)/;
+	
+	var toolsetHelper = new Toolset();
+	
+	if($(playerCell).text().indexOf("NPC") > -1) {
+		var npc = {
+			"name": null
+			, "npcTye": null
+			, "health": null
+			, "offence": {"player": null, "weapon": null}
+			, "defence": {"player": 0, "weapon": 0}
+		};
+		
+		$(playerCell).find("b:first").each(function(i,obj) {
+			npc.name = $(obj).text().trim();
+		});
+		
+		////////////////
+		// TEXT Nodes //
+		////////////////
+		this.getTextNodesIn(playerCell).each(function(i, textnode) {	
+			var textOfTextnode = $(textnode).text();
+			
+			//NPC Type and Health
+			if(textOfTextnode.indexOf("NPC") > -1) {
+				var headA = textOfTextnode.split(" LP: ");
+				var npcTye = headA[0];
+				npc.npcTye = npcTye;						
+				
+				if(npcTye.indexOf("Resistenz") > -1) {
+					npc.resistant = 1;
+				}
+				if(npcTye.indexOf("Superresistenz") > -1) {
+					npc.resistant = 2;
+				}
+				
+				if(headA.length > 1) {
+					var health = headA[1];
+					valueCurOfExpression.exec(health);
+					var currentHealth = RegExp.$1;
+					var maxHealth = RegExp.$3;
+					
+					currentHealth = currentHealth.replace("\.","")*1;
+					maxHealth = maxHealth.replace("\.","")*1;
+					
+					npc.health = {"current": currentHealth, "max": maxHealth};
+				}
+			}
+			else if(textOfTextnode.indexOf("Angriffsst") > -1) {
+				var parts = textOfTextnode.split(":");
+				var offence = parts[parts.length-1];
+				offence = offence.substring(0,offence.length-1);
+				offence = offence.replace("\.","")*1;
+				
+				npc.offence = {"player": offence, "weapon": 0};
+			}
+		});
+				
+		if(npc.health != null) {
+			var fight = toolsetHelper.fight(userdata,npc,0);
+			$(playerCell).find("a:last").after(fight.resultObj);
+		}
+		
+	}
 }
